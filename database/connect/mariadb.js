db82f7485111ec2e411891fbb4ea18ae0a311b3c
KimHyeonSu <hirobin2001@gmail.com>
2025-09-09 15:35:00 +0900

    First Commit

---
+++ database/connect/mariadb.js
@@ -0,0 +1,14 @@
+// mariadb를 연결할 세팅값 설정
+const mariadb = require('mysql');
+
+const conn = mariadb.createConnection(
+    {
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'Tennis'
+    }
+);
+
+module.exports = conn;
